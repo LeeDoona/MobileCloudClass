517f8f832224572e2b5151fa1d4f9e14ff743a97
LeeDoona(이두나) <nesk93@naver.com>
2016-05-10 22:19:34 +0900

    commit
    commit unreal engine project. it's tooooo big!

---
+++ PushPushBaby/Source/PushPushBaby/PushPushBabyGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PushPushBaby.h"
+#include "PushPushBabyGameMode.h"
+
+
+
+
---
+++ PushPushBaby/Source/PushPushBaby/PushPushBabyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PushPushBabyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUSHPUSHBABY_API APushPushBabyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
