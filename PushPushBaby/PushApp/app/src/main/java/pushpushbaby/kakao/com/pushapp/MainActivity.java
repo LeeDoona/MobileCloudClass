8f0c04e6d677d2b8e43c5ba50da76623a204dbcd
LeeDoona(이두나) <nesk93@naver.com>
2016-06-14 23:24:22 +0900

    unity android plugins

--- PushPushBaby/PushApp/app/src/main/java/pushpushbaby/kakao/com/pushapp/MainActivity.java
+++ PushPushBaby/PushApp/app/src/main/java/pushpushbaby/kakao/com/pushapp/MainActivity.java
@@ -3,11 +3,15 @@ package pushpushbaby.kakao.com.pushapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.unity3d.player.UnityPlayer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //call from native
+        UnityPlayer.UnitySendMessage("Main Camera", "CallFromNative", "NativeCallFromUnity call test");
     }
 }
