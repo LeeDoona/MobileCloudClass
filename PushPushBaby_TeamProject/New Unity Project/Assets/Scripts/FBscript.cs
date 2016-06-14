d9ae108e1613a644f799f844394e50a08085156a
LeeDoona(이두나) <nesk93@naver.com>
2016-06-15 06:55:28 +0900

    Facebook login

---
+++ PushPushBaby_TeamProject/New Unity Project/Assets/Scripts/FBscript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook.Unity;
+using System.Collections.Generic;
+
+public class FBscript : MonoBehaviour {
+
+	void Awake ()
+	{
+		FB.Init(SetInit, OnHideUnity);
+	}
+
+	void SetInit()
+	{
+		if(FB.IsLoggedIn ) {
+			Debug.Log("FB is logged in");
+		}else {
+			Debug.Log("FB is not logged in");
+			FBlogin ();
+		}
+	}
+
+	private void InitCallback ()
+	{
+		if (FB.IsInitialized) {
+			// Signal an app activation App Event
+			FB.ActivateApp();
+			// Continue with Facebook SDK
+			// ...
+		} else {
+			Debug.Log("Failed to Initialize the Facebook SDK");
+		}
+	}
+
+	private void OnHideUnity (bool isGameShown)
+	{
+		if (!isGameShown) {
+			// Pause the game - we will need to hide
+			Time.timeScale = 0;
+		} else {
+			// Resume the game - we're getting focus again
+			Time.timeScale = 1;
+		}
+	}
+
+	private void FBlogin()
+	{
+		var perms = new List<string>(){"public_profile", "email", "user_friends"};
+		FB.LogInWithReadPermissions(perms, AuthCallback);
+	}
+
+	private void AuthCallback (ILoginResult result) {
+		if (FB.IsLoggedIn) {
+			// AccessToken class will have session details
+			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+			// Print current access token's User ID
+			Debug.Log(aToken.UserId);
+			// Print current access token's granted permissions
+			foreach (string perm in aToken.Permissions) {
+				Debug.Log(perm);
+			}
+		} else {
+			Debug.Log("User cancelled login");
+		}
+	}
+
+}
